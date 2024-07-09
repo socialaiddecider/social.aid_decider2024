86dc87885a2c0c78b476e3bb021612c29bed3761
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-07-09 22:34:03 +0700

    feat(shared): add functionality to display news on landing page
    - Added code to retrieve all news articles
    - Passed news data to the view

--- app/Http/Controllers/SharedF/SharedController.php
+++ app/Http/Controllers/SharedF/SharedController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Shared;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Berita;
 
 class SharedController extends Controller
 {
     public function index()
     {
-        return view('shared.index');
+        $berita = Berita::all();
+        $data = [
+            'berita' => $berita,
+            'news' => $berita
+        ];
+
+        return view('shared.index', $data);
     }
 
 }
