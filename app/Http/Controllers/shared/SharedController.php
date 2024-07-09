76899021e6b4f793badb205db77cfc89066b769b
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-07-09 21:49:33 +0700

    fix(shared): controllers

--- app/Http/Controllers/shared/SharedController.php
+++ app/Http/Controllers/shared/SharedController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Shared;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Berita;
 
 class SharedController extends Controller
 {
     public function index()
     {
-        return view('shared.index');
+        $berita = Berita::all();
+        $data = [
+            'news' => $berita
+        ];
+
+        return view('shared.index', $data);
     }
 
 }
