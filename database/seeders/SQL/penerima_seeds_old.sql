bc30acfd7e3cd65cd92cf2a32599125b5e5134f0
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-09-03 00:28:04 +0700

    fix(database): seeder for alternatif, kriteria, penilaian, and normalisasi

---
+++ database/seeders/SQL/penerima_seeds_old.sql
@@ -0,0 +1,6 @@
+INSERT INTO `penerima` (`id`, `alternatif_id`, `nilai`, `status`, `created_at`, `updated_at`) VALUES
+(121, 1, 1.18619582686613, 'Menerima', '2024-04-30 17:00:00', NULL),
+(122, 5, 0.3400833002612166, 'Menerima', '2024-04-30 17:00:00', NULL),
+(123, 2, 1.01357254547669, 'Menerima', '2024-04-30 17:00:00', NULL),
+(124, 3, 0.6979043463983016, 'Menerima', '2024-04-30 17:00:00', NULL),
+(125, 6, 0.24948312552807259, 'Tidak Menerima', '2024-04-30 17:00:00', NULL);
